a06a6357bf78d344aab16ac98c69301a372d0806
Anish Kargaonkar <anish@onet.io>
2020-01-17 17:42:17 +0530

    feat: add file related functionalities

---
+++ src/ImageKit/File/File.php
@@ -0,0 +1,160 @@
+<?php
+namespace ImageKit\File;
+
+use ImageKit\Resource\GuzzleHttpWrapper;
+
+include_once __DIR__ . "/../Constants/errorMessages.php";
+include_once __DIR__ . "/../Utils/respond.php";
+
+class File
+{
+
+    // List File API
+    public function listFiles($parameters, $resource)
+    {
+        if (!is_array($parameters)) {
+            return respond(true, ((object) unserialize(INVALID_LIST_FILES_OPTIONS)));
+        }
+
+        $resource->setDatas((array) $parameters);
+        $res = $resource->get();
+        $stream = $res->getBody();
+        $content = $stream->getContents();
+
+        if ($res->getStatusCode() && $res->getStatusCode() !== 200) {
+            return respond(true, json_decode($content));
+        };
+
+        return respond(false, json_decode($content));
+    }
+
+    // Get Details Of file
+    public function getDetails($the_file_id, $resource)
+    {
+        if (empty($the_file_id)) {
+            return respond(true, ((object) unserialize(FILE_ID_MISSING)));
+        }
+
+        $res = $resource->get();
+        $stream = $res->getBody();
+        $content = $stream->getContents();
+
+        if ($res->getStatusCode() && $res->getStatusCode() !== 200) {
+            return respond(true, json_decode($content));
+        };
+
+        return respond(false, json_decode($content));
+
+    }
+
+    // Get Details Of file
+    public function getMetaData($the_file_id, $resource)
+    {
+        if (empty($the_file_id)) {
+            return respond(true, ((object) unserialize(FILE_ID_MISSING)));
+        }
+
+        $res = $resource->get();
+        $stream = $res->getBody();
+        $content = $stream->getContents();
+
+        if ($res->getStatusCode() && $res->getStatusCode() !== 200) {
+            return respond(true, json_decode($content));
+        };
+
+        return respond(false, json_decode($content));
+    }
+
+    // Delete File API
+    public function deleteFile($the_file_id, $resource)
+    {
+        if (empty($the_file_id)) {
+            return respond(true, ((object) unserialize(FILE_ID_MISSING)));
+        }
+
+        $resource->setDatas((array) $the_file_id);
+        $res = $resource->delete();
+        $stream = $res->getBody();
+        $content = $stream->getContents();
+
+        if ($res->getStatusCode() && $res->getStatusCode() !== 200) {
+            return respond(true, json_decode($content));
+        };
+
+        return respond(false, json_decode($content));
+
+    }
+
+    // Update File Details
+    public function updateDetails($the_file_id, $updateData, $resource)
+    {
+        if (empty($the_file_id)) {
+            return respond(true, ((object) unserialize(FILE_ID_MISSING)));
+        }
+
+        if (!is_array($updateData)) {
+            return respond(true, ((object) unserialize(UPDATE_DATA_MISSING)));
+        }
+
+        $obj = (object) $updateData;
+
+//        if (!isset($obj->tags) && ($obj->tags !== null) && !empty($obj->tags) && (is_array($obj->tags)) ) {
+        if (($obj->tags !== null) && ($obj->tags !== "undefined") && !is_array($obj->tags)) {
+            return respond(true, ((object) unserialize(UPDATE_DATA_TAGS_INVALID)));
+        }
+
+        if( ($obj->customCoordinates !== null) && ($obj->customCoordinates !== "undefined") && is_array($obj->customCoordinates)){
+            return respond(true, ((object) unserialize(UPDATE_DATA_COORDS_INVALID)));
+        }
+
+        $resource->setDatas($updateData);
+        $res = $resource->patch();
+        $stream = $res->getBody();
+        $content = $stream->getContents();
+
+        if ($res->getStatusCode() && $res->getStatusCode() !== 200) {
+            return respond(true, json_decode($content));
+        };
+
+        return respond(false, json_decode($content));
+    }
+
+
+    // purgeCache File API
+    public function purgeCacheApi($urlParam, $resource)
+    {
+        if (empty($urlParam)) {
+            return respond(true, ((object) unserialize(CACHE_PURGE_URL_MISSING)));
+        }
+
+        $resource->setDatas((array) $urlParam);
+        $res = $resource->post();
+        $stream = $res->getBody();
+        $content = $stream->getContents();
+
+        if ($res->getStatusCode() && !(200 >= $res->getStatusCode() || $res->getStatusCode() <= 300)) {
+            return respond(true, json_decode($content));
+        };
+
+        return respond(false, json_decode($content));
+    }
+
+    // purgeCache File API
+    public function purgeCacheApiStatus($requestId, GuzzleHttpWrapper $resource)
+    {
+        if (empty($requestId)) {
+            return respond(true, ((object) unserialize(CACHE_PURGE_STATUS_ID_MISSING)));
+        }
+
+            $res = $resource->get();
+
+            $stream = $res->getBody();
+            $content = $stream->getContents();
+
+            if ($res->getStatusCode() && $res->getStatusCode() !== 200) {
+                return respond(true, json_decode($content));
+            };
+
+            return respond(false, json_decode($content));
+    }
+}
---
+++ src/ImageKit/Upload/Upload.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace ImageKit\Upload;
+
+include_once __DIR__ . "/../Constants/errorMessages.php";
+include_once __DIR__ . "/../Utils/respond.php";
+
+class Upload
+{
+
+    public function uploadFileRequest(array $uploadOptions,  $resource)
+    {
+        $opts = (object) $uploadOptions;
+        if (!is_object($opts)) {
+            return respond(true, ((object) unserialize(MISSING_UPLOAD_DATA)));
+        }
+
+        if (empty($opts->file)) {
+            return respond(true, ((object) unserialize(MISSING_UPLOAD_FILE_PARAMETER)));
+        }
+
+        if (empty($opts->fileName)) {
+            return respond(true, ((object) unserialize(MISSING_UPLOAD_FILENAME_PARAMETER)));
+        }
+
+        $resource->setDatas((array) $opts);
+        $res = $resource->postMultipart();
+
+        $stream = $res->getBody();
+        $content = $stream->getContents();
+
+        if ($res->getStatusCode() && $res->getStatusCode() !== 200) {
+            return respond(true, json_decode($content));
+        };
+
+        return respond(false, json_decode($content));
+    }
+}

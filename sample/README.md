4e30a803c0b8fabc45d3a10aa85d3e0500acfaae
Sheheryar <sheheryarmuhammad5@gmail.com>
2022-06-13 16:51:09 +0500

    updated readme.md for sample app

---
+++ sample/README.md
@@ -0,0 +1,15 @@
+# Running the sample application
+
+### Step 1: Install dependencies
+Open `sample.php` and fill the account details:
+```php
+$public_key = 'your_public_key';
+$your_private_key = 'your_private_key';
+$url_end_point = 'https://ik.imagekit.io/demo';
+```
+
+### Step 2:
+Run the `sample.php`
+```bash
+php sample.php
+```
--- sample/sample.php
+++ sample/sample.php
@@ -8,13 +8,9 @@ if (php_sapi_name() !== 'cli') {
     exit;
 }
 
-// $public_key = 'your_public_key';
-// $your_private_key = 'your_private_key';
-// $url_end_point = 'https://ik.imagekit.io/demo';
-
-$public_key = 'public_Qp7LRq6BgZoqzQAzRpbUe2o68II=';
-$your_private_key = 'private_9d/vrR10y11DjFWsmKFb+ra2sHE=';
-$url_end_point = 'https://ik.imagekit.io/0wbiqzorc';
+$public_key = 'your_public_key';
+$your_private_key = 'your_private_key';
+$url_end_point = 'https://ik.imagekit.io/demo';
 
 $sample_file_url = 'https://cdn.pixabay.com/photo/2020/02/04/22/29/owl-4819550_960_720.jpg';
 $sample_file_path = '/default-image.jpg';

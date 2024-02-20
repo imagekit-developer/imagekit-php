6bacc62a267fcb3437c9a98c1ae03e8f1452f32b
ANKUR DWIVEDI <ankurdwivedi@ANKURs-MacBook-Pro-2.local>
2024-02-21 00:33:11 +0530

    set php version

--- src/ImageKit/ImageKit.php
+++ src/ImageKit/ImageKit.php
@@ -727,7 +727,7 @@ class ImageKit
         }
 
         $this->httpClient->setUri(Endpoints::getCopyFileEndpoint());
-        return Manage\File::copy($parameter['sourceFilePath'], $parameter['destinationPath'], $parameter['includeFileVersions']??false, $this->httpClient);
+        return Manage\File::copy($parameter['sourceFilePath'], $parameter['destinationPath'], isset($parameter['includeFileVersions']) ? $parameter['includeFileVersions'] : false, $this->httpClient);
     }
 
     
@@ -968,7 +968,7 @@ class ImageKit
         }
 
         $this->httpClient->setUri(Endpoints::getCopyFolderEndpoint());
-        return Manage\Folder::copy($parameter['sourceFolderPath'], $parameter['destinationPath'], $parameter['includeFileVersions']??false, $this->httpClient);
+        return Manage\Folder::copy($parameter['sourceFolderPath'], $parameter['destinationPath'], isset($parameter['includeFileVersions']) ? $parameter['includeFileVersions'] : false, $this->httpClient);
     }
 
     /**
@@ -1046,7 +1046,8 @@ class ImageKit
             return Response::respond(false, ($content));
         }
         else{
-            return Response::respond(true, ((object)ErrorMessages::$INVALID_REQUEST)->message);
+            $message = ErrorMessages::$INVALID_REQUEST->message;
+            return Response::respond(true, $message);
         }
     }
 
